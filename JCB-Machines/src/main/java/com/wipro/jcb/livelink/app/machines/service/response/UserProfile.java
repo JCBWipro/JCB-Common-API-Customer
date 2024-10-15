06898fc9a255359b61d7c880329a74129abd86dd
JitendraP1 <JitendraP1@verifone.com>
2024-10-15 18:16:38 +0530

    <Jitendra>updated Userprofile

--- JCB-Machines/src/main/java/com/wipro/jcb/livelink/app/machines/service/response/UserProfile.java
+++ JCB-Machines/src/main/java/com/wipro/jcb/livelink/app/machines/service/response/UserProfile.java
@@ -1,6 +1,6 @@
 package com.wipro.jcb.livelink.app.machines.service.response;
 
-import io.swagger.annotations.ApiModelProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 /**
@@ -15,25 +15,25 @@ import lombok.*;
 @NoArgsConstructor
 @ToString
 public class UserProfile {
-    @ApiModelProperty(value = "First name of user", example = "Leroy")
+    @Schema(description = "First name of user", example = "Leroy")
     private String firstName;
-    @ApiModelProperty(value = "Last name of user", example = "Holland")
+    @Schema(description ="Last name of user", example = "Holland")
     private String lastName;
-    @ApiModelProperty(value = "Email of user", example = "Leroy@mymail.com")
+    @Schema(description = "Email of user", example = "Leroy@mymail.com")
     private String email;
-    @ApiModelProperty(value = "contact of user", example = "9807867676")
+    @Schema(description = "contact of user", example = "9807867676")
     private String number;
-    @ApiModelProperty(value = "Image of user", example = "URL for Leroy.png")
+    @Schema(description = "Image of user", example = "URL for Leroy.png")
     private String image;
-    @ApiModelProperty(value = "Thumbnail image of user", example = "URL for Leroy.png")
+    @Schema(description = "Thumbnail image of user", example = "URL for Leroy.png")
     private String thumbnail;
-    @ApiModelProperty(value = "User nationality", example = "India")
+    @Schema(description = "User nationality", example = "India")
     private String country;
-    @ApiModelProperty(value = "Language decided by user", example = "English")
+    @Schema(description = "Language decided by user", example = "English")
     private String smsLanguage;
-    @ApiModelProperty(value = "Current timeZone ", example = "GMT+5:50")
+    @Schema(description = "Current timeZone ", example = "GMT+5:50")
     private String timeZone;
-    @ApiModelProperty(value = "User role ", example = "roleName of user")
+    @Schema(description = "User role ", example = "roleName of user")
     private String roleName;
 
 }
