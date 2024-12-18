d1556d6c2198b534075b29c90b656dd5d322aace
sabbasani259 <sabbasani.geetha@wipro.com>
2024-12-18 17:00:17 +0530

    Update before_install.sh

--- Scripts/before_install.sh
+++ Scripts/before_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "Cleaning up old artifacts..."
-rm -rf /data5/JCB_MobileAPI_Artifact/*
+# echo "Cleaning up old artifacts..."
+# rm -rf /data5/JCB_MobileAPI_Artifact/*
 echo "Preparing deployment directory..."
 mkdir -p /data5/JCB_MobileAPI_Artifacts
 echo "Setting execute permissions for scripts..."
