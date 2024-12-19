cae00605a34c3ed93a8a48c13f2077d674bae63c
JCBCommonAwsUser_Devops-at-779903482098 <sa20257979@wipro.com>
2024-12-19 15:15:56 +0530

    Set Permissions

--- Scripts/before_install.sh
+++ Scripts/before_install.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-echo "Cleaning up old artifacts..."
-rm -rf /data5/JCB_MobileAPI_Artifact/*
-echo "Preparing deployment directory..."
 mkdir -p /data5/JCB_MobileAPI_Artifacts
 echo "Setting execute permissions for scripts..."
 chmod +x /data5/JCB_MobileAPI_Artifacts/Scripts/*.sh
