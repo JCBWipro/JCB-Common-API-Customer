66a4158efa22a4a75cfba65082f03e849e21aced
sabbasani259 <sabbasani.geetha@wipro.com>
2024-12-21 20:37:43 +0530

    Update start_server.sh

--- Scripts/start_server.sh
+++ Scripts/start_server.sh
@@ -3,14 +3,16 @@ echo "Setting Permission for executing the jars"
 chmod +x /data5/JCB_MobileAPI_Artifacts/*.jar
 echo "Permissions set Successfully"
 echo "Starting Service Registry..."
-/data5/Softwares/jdk-17-linux/bin/java -jar /data5/JCB_MobileAPI_Artifacts/JCB-Service-Registry-0.0.1-SNAPSHOT.jar > /data5/JCB_MobileAPI_Artifact/service_registry.log 2>&1 &
+/data5/Softwares/jdk-17-linux/bin/java -jar /data5/JCB_MobileAPI_Artifacts/JCB-Service-Registry-0.0.1-SNAPSHOT.jar
 sleep 10
  
 echo "Starting other services..."
-/data5/Softwares/jdk-17-linux/bin/java -jar /data5/JCB_MobileAPI_Artifacts/JCB-API-Gateway-0.0.1-SNAPSHOT.jar > /data5/JCB_MobileAPI_Artifact/api_gateway.log 2>&1 &
-/data5/Softwares/jdk-17-linux/bin/java -jar /data5/JCB_MobileAPI_Artifacts/JCB-Authentication-0.0.1-SNAPSHOT.jar > /data5/JCB_MobileAPI_Artifact/authentication.log 2>&1 &
-/data5/Softwares/jdk-17-linux/bin/java -jar /data5/JCB_MobileAPI_Artifacts/JCB-Mobile-Authentication-0.0.1-SNAPSHOT.jar > /data5/JCB_MobileAPI_Artifact/mobile_auth.log 2>&1 &
-/data5/Softwares/jdk-17-linux/bin/java -jar /data5/JCB_MobileAPI_Artifacts/JCB-Machines-0.0.1-SNAPSHOT.jar > /data5/JCB_MobileAPI_Artifact/machines.log 2>&1 &
-/data5/Softwares/jdk-17-linux/bin/java -jar /data5/JCB_MobileAPI_Artifacts/JCB-User-0.0.1-SNAPSHOT.jar > /data5/JCB_MobileAPI_Artifact/user.log 2>&1 &
- 
+/data5/Softwares/jdk-17-linux/bin/java -jar /data5/JCB_MobileAPI_Artifacts/JCB-API-Gateway-0.0.1-SNAPSHOT.jar
+/data5/Softwares/jdk-17-linux/bin/java -jar /data5/JCB_MobileAPI_Artifacts/JCB-Authentication-0.0.1-SNAPSHOT.jar
+/data5/Softwares/jdk-17-linux/bin/java -jar /data5/JCB_MobileAPI_Artifacts/JCB-Mobile-Authentication-0.0.1-SNAPSHOT.jar 
+/data5/Softwares/jdk-17-linux/bin/java -jar /data5/JCB_MobileAPI_Artifacts/JCB-Machines-0.0.1-SNAPSHOT.jar
+/data5/Softwares/jdk-17-linux/bin/java -jar /data5/JCB_MobileAPI_Artifacts/JCB-User-0.0.1-SNAPSHOT.jar
+/data5/Softwares/jdk-17-linux/bin/java -jar /data5/JCB_MobileAPI_Artifacts/JCB-Reports-0.0.1-SNAPSHOT.jar
+/data5/Softwares/jdk-17-linux/bin/java -jar /data5/JCB_MobileAPI_Artifacts/JCB-Alerts-0.0.1-SNAPSHOT.jar
+
 echo "All services started successfully."
